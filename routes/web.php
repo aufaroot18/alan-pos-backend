c98c3ac9dbaa5692570517460a5fffb384c7451e
Aufa Billah <aufaroot18@gmail.com>
2019-11-06 05:34:59 +0700

    Kasir route: add route resource, print, and clear

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,8 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 // Barang routes
 route::resource('barang', 'BarangController')->middleware('auth');
+
+// Kasir
+route::get('kasir/print', 'KasirController@print')->name('kasir.print');
+route::delete('kasir/clear', 'KasirController@clear')->name('kasir.clear');
+route::resource('kasir', 'KasirController')->middleware('auth');
